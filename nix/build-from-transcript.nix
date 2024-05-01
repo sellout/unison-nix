0a5855aefc878d661fcbe594ccb5907ecf257812
Greg Pfeil <greg@technomadic.org>
2024-05-01 16:40:23 -0600

    Add a Nix formatter, and run it

--- nix/build-from-transcript.nix
+++ nix/build-from-transcript.nix
@@ -1,9 +1,14 @@
-{ cacert, lib, makeWrapper, stdenv, unison-ucm }:
-
-{ pname
-, version
-
-  /**
+{
+  cacert,
+  lib,
+  makeWrapper,
+  stdenv,
+  unison-ucm,
+}: {
+  pname,
+  version,
+  /*
+    *
   A Unison transcript file. The transcript should use the ucm `compile` command to compile any desired executables into the working directory.
 
   # Examples
@@ -18,9 +23,9 @@
     ''
   ````
   */
-, src
-
-  /**
+  src,
+  /*
+    *
   The compiledHash is the hash of the compiled Unison code. This is needed
   because Nix builds restrict network access unless the output hash is known
   ahead of time (which helps with reproducibility and caching). You won't know
@@ -28,17 +33,15 @@
   `pkgs.lib.fakeHash` and do a `nix build` or `nix run` and copy the hash
   labeled `got: `.
   */
-, compiledHash
-, meta ? { }
-}:
-
-let
+  compiledHash,
+  meta ? {},
+}: let
   compiled = stdenv.mkDerivation {
     # include the ucm version and transcript hash in the derivation name so it is rebuilt if either changes
     pname = pname + "_ucm-${unison-ucm.version}_${builtins.hashFile "sha256" src}";
     inherit version;
 
-    nativeBuildInputs = [ cacert ];
+    nativeBuildInputs = [cacert];
     buildCommand = ''
       export XDG_DATA_HOME="$TMP/.local/share"
       ${unison-ucm}/bin/ucm -C . transcript ${src}
@@ -50,18 +53,17 @@ let
     outputHashAlgo = "sha256";
     outputHash = compiledHash;
   };
-
 in
-stdenv.mkDerivation {
-  inherit pname version meta;
-  nativeBuildInputs = [ makeWrapper ];
-  buildInputs = [ ];
-  buildCommand = ''
-    mkdir -p $out/bin
+  stdenv.mkDerivation {
+    inherit pname version meta;
+    nativeBuildInputs = [makeWrapper];
+    buildInputs = [];
+    buildCommand = ''
+      mkdir -p $out/bin
 
-    for ucFile in ${compiled}/share/*.uc; do
-      makeWrapper "${unison-ucm}/bin/ucm" "$out/bin/$(basename "$ucFile" .uc)" \
-        --add-flags "run.compiled $ucFile"
-    done
-  '';
-}
+      for ucFile in ${compiled}/share/*.uc; do
+        makeWrapper "${unison-ucm}/bin/ucm" "$out/bin/$(basename "$ucFile" .uc)" \
+          --add-flags "run.compiled $ucFile"
+      done
+    '';
+  }
--- nix/build-share-project.nix
+++ nix/build-share-project.nix
@@ -1,27 +1,32 @@
-{ buildUnisonFromTranscript, lib }:
-
-/**
+{
+  buildUnisonFromTranscript,
+  lib,
+}:
+/*
+  *
 Compile functions from a project hosted on Unison Share into executables.
 */
-{ pname
-, version
-
-  /**
+{
+  pname,
+  version,
+  /*
+    *
   The Unison Share user handle of owner of the project to compile.
   For example in the project `@unison/base` the user handle is `unison`.
   */
-, userHandle
-
-  /**
+  userHandle,
+  /*
+    *
   The name of the project to compile.
   For example in the project `@unison/base` the project name is `base`.
   */
-, projectName
-
-  /** A release version of the project to compile (ex: `3.1.2`). */
-, projectReleaseVersion ? args.version
-
-  /**
+  projectName,
+  /*
+  * A release version of the project to compile (ex: `3.1.2`).
+  */
+  projectReleaseVersion ? args.version,
+  /*
+    *
   The compiledHash is the hash of the compiled Unison code. This is needed
   because Nix builds restrict network access unless the output hash is known
   ahead of time (which helps with reproducibility and caching). You won't know
@@ -29,20 +34,19 @@ Compile functions from a project hosted on Unison Share into executables.
   `pkgs.lib.fakeHash` and do a `nix build` or `nix run` and copy the hash
   labeled `got: `.
   */
-, compiledHash
-
-  /**
+  compiledHash,
+  /*
+    *
   A list of executables to compile. Each key is an executable name and each
   value is a fully-qualified entry function name. For example, `snake =
   examples.snake.main` will generate a `/bin/snake` executable out of the
   function `examples.snake.main : '{IO, Exception} ()`.
   */
-, executables ? { pname = "main"; }
-, meta ? { }
-}@args :
-
-let
-  compileCommands = lib.attrsets.mapAttrsToList
+  executables ? {pname = "main";},
+  meta ? {},
+} @ args: let
+  compileCommands =
+    lib.attrsets.mapAttrsToList
     (executableName: functionName: "tmp/main> compile ${functionName} ${executableName}")
     executables;
 
@@ -53,10 +57,9 @@ let
     ${lib.strings.concatStringsSep "\n" compileCommands}
     ```
   '';
-
 in
-buildUnisonFromTranscript {
-  inherit pname version compiledHash meta;
+  buildUnisonFromTranscript {
+    inherit pname version compiledHash meta;
 
-  src = builtins.toFile "${pname}-compile-transcript-${version}.md" transcript;
-}
+    src = builtins.toFile "${pname}-compile-transcript-${version}.md" transcript;
+  }
